10a903f2694400d096a55ee25fa0654d3c90af5e
Randal Carr <randalrcarr@gmail.com>
2021-12-24 17:44:20 -0600

    Added Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,6 +3,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import runners from './images/running1.jpeg';
 import macbook from './images/macbook1.jpeg'
+import useStyles from './styles';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: runners},
@@ -10,10 +11,12 @@ const products = [
 ];
 
 const Products = ()=> {
+    const classes = useStyles();
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing= {4}>
-                {products.map((product) => (   //loop through our products array
+                {products.map((product) => (   //loop through our products ß
                     <Grid item key = {product.id} xs ={12} sm={6} md={4} lg= {3}>
                         <Product product={product}/>
                     </Grid>
