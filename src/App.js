8d6c00f9a531ec6d1caaf7e9116117aa09c9817e
Randal Carr <randalrcarr@gmail.com>
2021-12-30 15:12:43 -0600

    adding countries to checkout form

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     }
     const handleAddToCart = async (product_id, quantity ) => {
         const { cart } = await commerce.cart.add(product_id, quantity);
-        setCart(cart);
+        setCart(cart); 
     }
 
     const handleUpdateCartQty = async (product_id, quantity) => {
@@ -51,7 +51,7 @@ const App = () => {
                     <Route exact path="/" element={<Products products={products} onAddToCart={handleAddToCart}/>}></Route>
                     <Route exact path = "/cart" element= {<Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart}/>}> 
                     </Route>
-                    <Route exat path="/checkout" element={ <Checkout />}> </Route>
+                    <Route exat path="/checkout" element={ <Checkout cart ={cart} />}> </Route>
                 </Routes>
             </div>
         </Router>
---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,74 @@
+import { React, useState, useEffect } from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography, Input } from '@material-ui/core';
+import {useForm, FormProvider} from 'react-hook-form';
+import FormInput from './FormInput';
+import { commerce } from '../../lib/commerce';
+
+const AddressForm = ( { checkoutToken }) => {
+
+    const methods = useForm();
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubDivisions, setShippingSubDivisions] = useState([]);
+    const [shippingSubDivision, setShippingSubDivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
+
+    const countries =  Object.entries(shippingCountries).map(([code, name]) => ({id: code, label: name}));
+
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+        setShippingCountries(countries);
+        setShippingCountry(Object.keys(countries)[0]); /* get the keys of all countries, than the first country */
+    }
+
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id);
+    }, []);
+
+    return (
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name='firstName' label='First Name'/>
+                        <FormInput required name='lastName' label='Last Name'/>
+                        <FormInput required name='address1' label='Address'/>
+                        <FormInput required name='email' label='Email'/>
+                        <FormInput required name='city' label='City'/>
+                        <FormInput required name='zip' label='Zip Code'/>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                                {countries.map((country) => (
+                                    <MenuItem key={country.id} value={country.name}>
+                                        {country.label}
+                                    </MenuItem>))}
+                            </Select>
+                        </Grid>
+                        {/*<Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                            </Grid>*/}
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
+    )
+}
+
+export default AddressForm
